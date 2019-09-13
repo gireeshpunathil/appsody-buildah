d62087b26faf221823e582f462e82afb4ea3c68a
Gireesh Punathil <gpunathi@in.ibm.com>
2019-09-13 09:40:31 +0530

    extract: perform composition and push too
    As the next step in the pipeline is to recompose the application
    and push to the registry (which is performed by kaniko atm),
    and as buildah is very much capable of doing that, and as we are
    already in the context, perform extraction, recomposition and
    push here itself.

--- extract.sh
+++ extract.sh
@@ -6,4 +6,9 @@ cd /workspace/$gitsource
 # Extract everything into /workspace/extracted, the image
 # content goes to the vortex, content in the current
 # folder into user-app, and the rest, as per the mounts.
-appsody extract --buildah --target-dir /workspace/extracted -v
+appsody extract --buildah --target-dir $path_to_context
+
+# Compose and push the image to the registry
+cd $path_to_context
+imageid=`buildah bud --format=docker -t $image .`
+buildah push --tls-verify=false $imageid $image
